930607741363cf02378c3067bcb1c03e1cd08533
jihad <jihad_ashor@yahoo.com>
2024-10-06 08:32:00 +0300

    newww

--- project/project/urls.py
+++ project/project/urls.py
@@ -1,3 +1,4 @@
+from django.conf.urls import include
 """
 URL configuration for project project.
 
@@ -19,4 +20,5 @@ from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('pages.urls'))
 ]
